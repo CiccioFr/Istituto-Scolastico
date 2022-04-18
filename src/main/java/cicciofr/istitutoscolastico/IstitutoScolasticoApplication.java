e6be08ff3dc590c92a746725bae0749eaefa887b
CiccioFr <cicciofr.net@tiscali.it>
2022-04-18 22:48:34 +0200

    Creazione Progetto Istituto Scolastico

---
+++ src/main/java/cicciofr/istitutoscolastico/IstitutoScolasticoApplication.java
@@ -0,0 +1,13 @@
+package cicciofr.istitutoscolastico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstitutoScolasticoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IstitutoScolasticoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cicciofr/istitutoscolastico/IstitutoScolasticoApplicationTests.java
@@ -0,0 +1,13 @@
+package cicciofr.istitutoscolastico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstitutoScolasticoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
